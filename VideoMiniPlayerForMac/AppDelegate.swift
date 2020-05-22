c616fef2cacbfc34fc07d427da156c749836108d
Mitsuki Goto <minionnzu20041125@yahoo.co.jp>
2020-05-22 22:57:57 +0900

    initalized commit

---
+++ VideoMiniPlayerForMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VideoMiniPlayerForMac
+//
+//  Created by MitsukiGoto on 2020/05/22.
+//  Copyright © 2020 MitsukiGoto. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ VideoMiniPlayerForMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VideoMiniPlayerForMac
+//
+//  Created by MitsukiGoto on 2020/05/22.
+//  Copyright © 2020 MitsukiGoto. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
